57145d556e566b1e4cfbc4a6b71330cb1b4065b9
climax-solution <blocklimax@gmail.com>
2021-10-26 18:12:06 +1300

    migration reconfig

--- migrations/2_deploy_PhotoNFTMarketPlace.js
+++ migrations/2_deploy_PhotoNFTMarketPlace.js
@@ -2,9 +2,9 @@ const PhotoNFTMarketplace = artifacts.require("./PhotoNFTMarketplace.sol");
 const PhotoNFTData = artifacts.require("./PhotoNFTData.sol");
 
 const _photoNFTData = PhotoNFTData.address;
-const _marketplaceOwner = '0x211A5f4D4A72FDDe0f7Ce2b02C25a011aEf85ca4';
+const _marketplaceOwner = '0xebc6B3c6F7724BB214b7CF5994078BB883208a98';
 //setowner
-
+// 0xebc6B3c6F7724BB214b7CF5994078BB883208a98
 module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(PhotoNFTMarketplace, _photoNFTData, _marketplaceOwner);
 };
