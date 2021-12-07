65a87adaae5b5323fc85fb696fda31e1011a91c7
climax-solution <blocklimax@gmail.com>
2021-12-07 08:00:19 +0300

    update migrations

--- migrations/1_deploy_PhotoNFT.js
+++ migrations/1_deploy_PhotoNFT.js
@@ -1,5 +1,6 @@
 const PhotoNFT = artifacts.require("./PhotoNFT.sol");
+const owner = "0x3530e9D7E112b78c4778Be38C83708a9F8f7223B";
 
 module.exports = async function(deployer, network, accounts) {
-    await deployer.deploy(PhotoNFT);
+    await deployer.deploy(PhotoNFT, owner);
 };
