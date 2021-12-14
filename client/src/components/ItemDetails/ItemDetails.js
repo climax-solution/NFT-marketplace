c22779dfb24569ab0cac928d31911ab45acd3a26
climax-solution <blocklimax@gmail.com>
2021-12-14 17:45:38 +0300

    update item details page

--- client/src/components/ItemDetails/ItemDetails.js
+++ client/src/components/ItemDetails/ItemDetails.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import getWeb3 from '../../utils/getWeb3';
-import { zeppelinSolidityHotLoaderOptions } from '../../../config/webpack';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
 import addresses from "../../config/address.json";
+import ItemLoading  from "../Loading/itemLoading";
 
-const { marketplace_addr, nft_addr, token_addr } = addresses;
+const { marketplace_addr, nft_addr } = addresses;
 
 class ItemDetails extends Component {
 
@@ -12,7 +12,8 @@ class ItemDetails extends Component {
         super(props);
         this.state = {
           web3: null,
-          itemData: {}
+          itemData: {},
+          isLoading: true
         }
     }
 
@@ -86,57 +87,60 @@ class ItemDetails extends Component {
         } catch (error) {
             console.error(error);
         }
+        this.setState({ isLoading: false });
       };
 
     render() {
-        const { itemData, web3 } = this.state;
+        const { itemData, web3, isLoading } = this.state;
         return (
             <>
             <Breadcrumb title="NFT ITEM"/>
+            { isLoading  && <ItemLoading/> }
             {
-            Object.keys(itemData).length ?
-                <section className="item-details-area">
-                    <div className="container">
-                        <div className="row justify-content-between">
-                            <div className="col-12 col-lg-5">
-                                <div className="item-info">
-                                    <div className="item-thumb text-center p-md-4 p-3" style={{background: "rgba(255,255,255,0.1)"}}>
-                                        <img src={`${process.env.REACT_APP_IPFS}/ipfs/${itemData.image}`} alt="" />
+                !isLoading && (
+                Object.keys(itemData).length ?
+                    <section className="item-details-area">
+                        <div className="container">
+                            <div className="row justify-content-between">
+                                <div className="col-12 col-lg-5">
+                                    <div className="item-info">
+                                        <div className="item-thumb text-center p-md-4 p-3" style={{background: "rgba(255,255,255,0.1)"}}>
+                                            <img src={`${process.env.REACT_APP_IPFS}/ipfs/${itemData.image}`} alt="" />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-12 col-lg-6">
-                                {/* Content */}
-                                <div className="content mt-5 mt-lg-0">
-                                    <h3 className="m-0">{itemData.nftName}</h3>
-                                    {/* Owner */}
-                                    <div className="owner d-flex align-items-center mt-4">
-                                        <span>Owner : </span>
-                                        <a href={`https://bscscan.com/address/${itemData.nftData.owner}`} target="_blank" className="ml-2">{itemData.nftData.owner.substr(0,14) + "..." + itemData.nftData.owner.substr(-4)}</a>
+                                <div className="col-12 col-lg-6">
+                                    {/* Content */}
+                                    <div className="content mt-5 mt-lg-0">
+                                        <h3 className="m-0">{itemData.nftName}</h3>
+                                        {/* Owner */}
+                                        <div className="owner d-flex align-items-center mt-4">
+                                            <span>Owner : </span>
+                                            <a href={`https://bscscan.com/address/${itemData.nftData.owner}`} target="_blank" className="ml-2">{itemData.nftData.owner.substr(0,14) + "..." + itemData.nftData.owner.substr(-4)}</a>
+                                        </div>
+                                        {/* Item Info List */}
+                                        <div className="item-info-list mt-4">
+                                            <ul className="list-unstyled">
+                                                <li className="price d-flex justify-content-between">
+                                                    <span>Current Price :  {web3.utils.fromWei(`${itemData.marketData.price}`,"ether")} BNB</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div className="item-info-list mt-4">
+                                            <ul className="list-unstyled">
+                                                <li className="price d-flex justify-content-between">
+                                                    <span>Premium NFT :  {itemData.marketData.premiumStatus ? "YES" : "NO"}</span>
+                                                </li>
+                                            </ul>
+                                        </div>  
+                                        <p>Description:</p>
+                                        <p>{itemData.nftDesc}</p>
                                     </div>
-                                    {/* Item Info List */}
-                                    <div className="item-info-list mt-4">
-                                        <ul className="list-unstyled">
-                                            <li className="price d-flex justify-content-between">
-                                                <span>Current Price :  {web3.utils.fromWei(`${itemData.marketData.price}`,"ether")} BNB</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className="item-info-list mt-4">
-                                        <ul className="list-unstyled">
-                                            <li className="price d-flex justify-content-between">
-                                                <span>Premium NFT :  {itemData.marketData.premiumStatus ? "YES" : "NO"}</span>
-                                            </li>
-                                        </ul>
-                                    </div>  
-                                    <p>Description:</p>
-                                    <p>{itemData.nftDesc}</p>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-                : <h3 className="text-muted ml-3">No existed item</h3>
+                    </section>
+                    : <h3 className="text-muted ml-3">No existed item</h3>)
             }
             </>
         );
