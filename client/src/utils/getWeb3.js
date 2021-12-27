22da89e5f5784ebd16b15cadd8893daff6dd5770
climax-solution <blocklimax@gmail.com>
2021-12-27 17:27:59 +0300

    update web3 config

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -1,22 +1,28 @@
 import Web3 from "web3";
 
+const node = [
+  'https://bsc-dataseed.binance.org',
+  'https://bsc-dataseed1.binance.org',
+  'https://bsc-dataseed2.binance.org',
+  'https://bsc-dataseed3.binance.org',
+  'https://bsc-dataseed4.binance.org'
+]
+
 const getWeb3 = async() => {
-  let web3;
+  let web3; let ranId;
   if (window.ethereum) {
     web3 = new Web3(window.ethereum);
     const networkId = await web3.eth.net.getId();
-    if (networkId != 3) {
-      const provider = new Web3.providers.HttpProvider(
-        'https://ropsten.infura.io/v3/e5f6b05589544b1bb8526dc3c034c63e'
-      );
+    if (networkId != 56) {
+      ranId = Math.floor(Math.random() * 5);
+      const provider = new Web3.providers.HttpProvider(node[ranId]);
       web3 = new Web3(provider);
     }
   }
 
   else {
-    const provider = new Web3.providers.HttpProvider(
-      'https://ropsten.infura.io/v3/e5f6b05589544b1bb8526dc3c034c63e'
-    );
+    ranId = Math.floor(Math.random() * 5);
+    const provider = new Web3.providers.HttpProvider(node[ranId]);
     web3 = new Web3(provider);
     //console.log("No web3 instance injected, using Infura/Local web3.");
   }
