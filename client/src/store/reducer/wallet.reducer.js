3ff7ccf6b8f1ac55f30c4285f8eeeb99fe901c0e
climax-solution <blocklimax@gmail.com>
2022-01-24 04:53:33 +0200

    update reducer

--- client/src/store/reducer/wallet.reducer.js
+++ client/src/store/reducer/wallet.reducer.js
@@ -8,9 +8,7 @@ export default function (state = status, action) {
     switch(action.type) {
         case WALLET_CONNECT:
             return {...state, wallet_connected: action.payload}
-            break;
         default:
             return {...state};
-            break;
     }
 }
