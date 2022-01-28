69ff1e44f90bcad2e86aa075d62911e40395fc2a
climax-solution <blocklimax@gmail.com>
2022-01-28 21:16:35 +0200

    update regsiter page

--- frontend/src/components/pages/register.js
+++ frontend/src/components/pages/register.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
+import CountryDropdown from 'country-dropdown-with-flags-for-react';
+import Footer from '../components/footer';
 
 const GlobalStyles = createGlobalStyle`
   header#myHeader.navbar.white {
     background: #212428;
+  }
+  .country-select {
+      display: block;
   }
 `;
 
@@ -25,60 +29,74 @@ const register= () => (
 
       <form name="contactForm" id='contact_form' className="form-border" action='#'>
 
-                        <div className="row">
+            <div className="row">
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Name:</label>
-                                    <input type='text' name='name' id='name' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Name:</label>
+                        <input type='text' name='name' id='name' className="form-control"/>
+                    </div>
+                </div>
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Email Address:</label>
-                                    <input type='text' name='email' id='email' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Birth date:</label>
+                        <input type='date' name='birth-date' id='birth-date' className="form-control"/>
+                    </div>
+                </div>
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Choose a Username:</label>
-                                    <input type='text' name='username' id='username' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Country:</label>
+                        <CountryDropdown  id="country-list" className="form-control" preferredCountries={['nz', 'us']}  value={""} handleChange={e => console.log(e.target.value)}/>
+                    </div>
+                </div>
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Phone:</label>
-                                    <input type='text' name='phone' id='phone' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Email Address:</label>
+                        <input type='text' name='email' id='email' className="form-control"/>
+                    </div>
+                </div>
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Password:</label>
-                                    <input type='text' name='password' id='password' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Choose a Username:</label>
+                        <input type='text' name='username' id='username' className="form-control"/>
+                    </div>
+                </div>
 
-                            <div className="col-md-6">
-                                <div className="field-set">
-                                    <label>Re-enter Password:</label>
-                                    <input type='text' name='re-password' id='re-password' className="form-control"/>
-                                </div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Phone:</label>
+                        <input type='text' name='phone' id='phone' className="form-control"/>
+                    </div>
+                </div>
 
-                            <div className="col-md-12">
-                                <div id='submit' className="pull-left">
-                                    <input type='submit' id='send_message' value='Register Now' className="btn btn-main color-2" />
-                                </div>
-                                
-                                <div className="clearfix"></div>
-                            </div>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Password:</label>
+                        <input type='text' name='password' id='password' className="form-control"/>
+                    </div>
+                </div>
 
-                        </div>
-                    </form>
+                <div className="col-md-6">
+                    <div className="field-set">
+                        <label>Re-enter Password:</label>
+                        <input type='text' name='re-password' id='re-password' className="form-control"/>
+                    </div>
+                </div>
+
+                <div className="col-md-12">
+                    <div id='submit' className="pull-left">
+                        <input type='submit' id='send_message' value='Register Now' className="btn btn-main color-2" />
+                    </div>
+                    
+                    <div className="clearfix"></div>
+                </div>
+
+            </div>
+        </form>
       </div>
 
     </div>
