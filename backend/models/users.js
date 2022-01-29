6bbaf12bd3534956519f8b944768c0c693619468
climax-solution <blocklimax@gmail.com>
2022-01-29 04:39:49 +0200

    add model - User

---
+++ backend/models/users.js
@@ -0,0 +1,59 @@
+import Mongoose from "mongoose";
+
+const { Schema } = Mongoose;
+
+const UserSchema = Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: () => {
+            return this.provider !== 'email' ? false : true
+        }
+    },
+    phoneNumber: {
+        type: String
+    },
+    walletAddress: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    facebookID: {
+        type: String
+    },
+    instagramID: {
+        type: String
+    },
+    role: {
+        type: String,
+        default: 'ROLE_VISITOR',
+        enum: ['ROLE_VISITOR', 'ROLE_CREATOR', 'ROLE_ADMIN']
+    },
+    resetPasswordToken: {
+        type: String
+    },
+    resetPasswordExpires: {
+        type: Date
+    },
+    updated_at: {
+        type: Date
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Mongoose.model('User', UserSchema);
