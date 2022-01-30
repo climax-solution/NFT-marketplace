61501d15869fadf5fdd3751caca21072d672177c
climax-solution <blocklimax@gmail.com>
2022-01-30 19:33:52 +0200

    update login and register

--- frontend/src/components/pages/login.js
+++ frontend/src/components/pages/login.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
 
@@ -15,42 +17,100 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-const logintwo= () => (
-<div>
-<GlobalStyles/>
+const login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailStatus, setEmailStatus] = useState('');
+  const [passwordStatus, setPasswordStatus] = useState('');
 
-  <section className='jumbotron breadcumb no-bg' style={{backgroundImage: `url(${'./img/background/6.jpg'})`}}>
-    <div className='mainbreadcumb'>
-      <div className='container'>
-        <div className='row align-items-center px-0'>
-          <div className="col-lg-4 offset-lg-4 m-auto px-0">
-            <div className="box-login">
-              <h3 className="mb10">Sign In</h3>
-              <p>Login using an existing account or create a new account <span>here</span>.</p>
-              <form name="contactForm" id='contact_form' className="form-border" action='#'>
+  const authenticate = async() => {
+    let flag = 0;
+    if (!email) {
+      setEmailStatus("Please enter correctly");
+      flag = 1;
+    }
+    else setEmailStatus("");
 
-                  <div className="field-set">
-                      <input type='text' name='email' id='email' className="form-control" placeholder="username"/>
+    if (!password) {
+      setPasswordStatus("Pleaes enter correctly");
+      flag = 1;
+    }
+    else setPasswordStatus("");
+    if (flag) return;
+    const data = {
+      id: email,
+      password
+    };
+    await axios.post("http://localhost:7060/auth/login", data).then(res => {
+      toast.success("You have logined successfully", {theme: "colored"});
+      const { token } = res.data;
+      localStorage.setItem("nftdevelopments-token", JSON.stringify(token));
+    }).catch(err => {
+      const { error } = err.response.data;
+      toast.error(error, {theme: "colored"});
+    })
+  }
+
+  return(
+    <div>
+    <GlobalStyles/>
+
+      <section className='jumbotron breadcumb no-bg' style={{backgroundImage: `url(${'./img/background/6.jpg'})`}}>
+        <div className='mainbreadcumb'>
+          <div className='container'>
+            <div className='row align-items-center px-0'>
+              <div className="col-lg-4 offset-lg-4 m-auto px-0">
+                <div className="box-login">
+                  <h3 className="mb10">Sign In</h3>
+                  <p>Login using an existing account or create a new account <span>here</span>.</p>
+                  <div name="contactForm" id='contact_form' className="form-border" action='#'>
+
+                    <div className="field-set">
+                      <input
+                        type='text'
+                        name='email'
+                        id='email'
+                        className="form-control"
+                        placeholder="Please enter your email or username"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <label className='text-danger'>{emailStatus}</label>
+                    </div>
+                    
+                    <div className="field-set">
+                      <input
+                        type='password'
+                        name='password'
+                        id='password'
+                        className="form-control"
+                        placeholder="Please enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <label className='text-danger'>{passwordStatus}</label>
+                    </div>
+                    
+                    <div className="field-set">
+                      <input
+                        type='submit'
+                        id='send_message'
+                        value='Login'
+                        className="btn btn-main btn-fullwidth color-2"
+                        onClick={authenticate}
+                      />
+                    </div>
+                    <div className="spacer-half"></div>
                   </div>
-                
-                 <div className="field-set">
-                      <input type='password' name='password' id='password' className="form-control" placeholder="password"/>
-                  </div>
-                
-                <div className="field-set">
-                  <input type='submit' id='send_message' value='Submit' className="btn btn-main btn-fullwidth color-2"/>
                 </div>
-                <div className="spacer-half"></div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
+
+      <Footer />
     </div>
-  </section>
-
-  <Footer />
-</div>
-
-);
-export default logintwo;
+  )
+};
+export default login;
--- frontend/src/components/pages/register.js
+++ frontend/src/components/pages/register.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { phone } from "phone";
 import { createGlobalStyle } from 'styled-components';
+import { toast } from 'react-toastify';
 import CountryDropdown from 'country-dropdown-with-flags-for-react';
 import PhoneInput from 'react-phone-input-2'
+import walletValidator from 'wallet-address-validator';
+import EmailValidator from 'email-validator';
 import Footer from '../components/footer';
 import 'react-phone-input-2/lib/style.css';
 
@@ -41,12 +46,124 @@ const GlobalStyles = createGlobalStyle`
 
 
 const register= () => {
-        
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email ,setEmail] = useState('');
+    const [username, setUserName] = useState('');
+    const [birthday, setBirthDate] = useState('');
+    const [country, setCountry] = useState('');
+    const [phoneNumber ,setPhoneNumber] = useState('');
+    const [walletAddress, setWalletAddres] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    const [firstNameStatus, setFirstNameStatus] = useState('');
+    const [lastNameStatus, setLastNameStatus] = useState('');
+    const [emailStatus ,setEmailStatus] = useState('');
+    const [usernameStatus, setUserNameStatus] = useState('');
+    const [birthdayStatus, setBirthDateStatus] = useState('');
+    const [countryStatus, setCountryStatus] = useState('');
+    const [phoneNumberStatus ,setPhoneNumberStatus] = useState('');
+    const [walletAddressStatus, setWalletAddresStatus] = useState('');
+    const [passwordStatus, setPasswordStatus] = useState('');
+    const [confirmPasswordStatus, setConfirmPasswordStatus] = useState('');
+
+    const register = async() => {
+        let flag = 0;
+        if (!firstName) {
+            setFirstNameStatus("Please enter your first name.");
+            flag = 1;
+        } else setFirstNameStatus("");
+
+        if (!lastName) {
+            setLastNameStatus("Please enter your last name.");
+            flag = 1;
+        } else setLastNameStatus("");
+
+        if (!email || !EmailValidator.validate(email)) {
+            setEmailStatus("Please enter your correct email.");
+            flag = 1;
+        } else setEmailStatus("");
+
+        if (!username) {
+            setUserNameStatus("Please enter your user name");
+            flag = 1;
+        } else setUserNameStatus("");
+
+        if (!birthday) {
+            setBirthDateStatus("Please enter your birthday");
+            flag = 1;
+        } else setBirthDateStatus("");
+
+        if (!country) {
+            setCountryStatus("Please enter your country");
+            flag = 1;
+        } else setCountryStatus("");
+
+        if (!phoneNumber) {
+            setPhoneNumberStatus("Please enter your phone number");
+            flag = 1;
+        } else {
+            const res = phone(phoneNumber);
+            if (!res.isValid) {
+                setPhoneNumberStatus("Please enther your correct phone number.");
+                flag = 1;
+            }
+            else setPhoneNumberStatus("");
+        }
+
+        if (!walletAddress) {
+            setWalletAddresStatus("Please enter your wallet address");
+            flag = 1;
+        } else {
+            if (!walletValidator.validate(walletAddress, "ETH")) {
+                setWalletAddresStatus("Please enter your correct wallet address");
+                flag = 1;
+            } else setWalletAddresStatus("");
+        }
+
+        if (!password) {
+            setPasswordStatus("Please enter your password");
+            flag = 1;
+        } else setPasswordStatus("");
+
+        if (!confirmPassword || password && confirmPassword && password !== confirmPassword) {
+            setConfirmPasswordStatus("Please confirm your password");
+            flag = 1;
+        } else setConfirmPasswordStatus("");
+
+        if (flag) return;
+
+        const data = {
+            username,
+            email,
+            firstName,
+            lastName,
+            birthday,
+            phoneNumber,
+            country,
+            walletAddress,
+            password
+        };
+
+        await axios.post("http://localhost:7060/auth/register", data).then(res => {
+           const { sucess, token } = res.data;
+           toast.success("You have registered successfully!", { theme: "colored" });
+
+        }).catch(err => {
+            const { error } = err.response.data;
+            toast.error(error, { theme: "colored" });
+        })
+    }
+
+    const handleChangePhone = (value) => {
+        setPhoneNumber("+"+value);
+    }
+
     return (
         <div>
             <GlobalStyles />
-
-
             <section className='container'>
                 <div className="row">
                 <div className='spacer-double'></div>
@@ -56,49 +173,80 @@ const register= () => {
 
                 <div className="spacer-10"></div>
 
-                <form name="contactForm" id='contact_form' className="form-border" action='#'>
+                <div name="contactForm" id='contact_form' className="form-border" action='#'>
 
                         <div className="row">
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>First Name:</label>
-                                    <input type='text' name='first-name' id='first-name' className="form-control"/>
+                                    <input type='text' name='first-name' id='first-name' className="form-control mb-0" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                                    {
+                                        <label className='text-danger'>{firstNameStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Last Name:</label>
-                                    <input type='text' name='last-name' id='last-name' className="form-control"/>
+                                    <input type='text' name='last-name' id='last-name' className="form-control mb-0" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                                    {
+                                        <label className='text-danger'>{lastNameStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Email Address:</label>
-                                    <input type='text' name='email' id='email' className="form-control"/>
+                                    <input type='email' name='email' id='email' className="form-control mb-0" value={email} onChange={(e) => setEmail(e.target.value) }/>
+                                    {
+                                        <label className='text-danger'>{emailStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Choose a Username:</label>
-                                    <input type='text' name='username' id='username' className="form-control"/>
+                                    <input type='text' name='username' id='username' className="form-control mb-0" value={username} onChange={(e) => setUserName(e.target.value)}/>
+                                    {
+                                        <label className='text-danger'>{usernameStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Birth date:</label>
-                                    <input type='date' name='birth-date' id='birth-date' className="form-control"/>
+                                    <input type='date' name='birth-date' id='birth-date' className="form-control mb-0" value={birthday} onChange={(e) => setBirthDate(e.target.value) }/>
+                                    {
+                                        <label className='text-danger'>{birthdayStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Country:</label>
-                                    <CountryDropdown  id="country-list" className="form-control" preferredCountries={['nz', 'us']}  value={""} handleChange={e => console.log(e.target.value)}/>
+                                    <CountryDropdown
+                                        id="country-list"
+                                        className="form-control mb-0"
+                                        preferredCountries={['nz', 'us']}
+                                        value={country}
+                                        handleChange={e => setCountry(e.target.value)}
+
+                                        isValid={(inputNumber, country, countries) => {
+                                            return countries.some((country) => {
+                                              return inputNumber;
+                                            });
+                                          }}
+                                        
+                                    />
+                                    {
+                                        <label className='text-danger'>{countryStatus}</label>
+                                    }
                                 </div>
                             </div>
 
@@ -110,45 +258,58 @@ const register= () => {
                                         inputProps={{
                                             name: 'phone',
                                             required: true,
-                                            autoFocus: true
                                         }}
                                         className="w-100"
                                         enableSearch={true}
+                                        value={phoneNumber}
+                                        onChange={handleChangePhone}
                                     />
+                                    {
+                                        <label className='text-danger'>{phoneNumberStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Wallet Address:</label>
-                                    <input type='text' name='wallet-address' id='wallet-address' className="form-control"/>
+                                    <input type='text' name='wallet-address' id='wallet-address' className="form-control mb-0" value={walletAddress} onChange={(e) => setWalletAddres(e.target.value) }/>
+                                    {
+                                        <label className='text-danger'>{walletAddressStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Password:</label>
-                                    <input type='text' name='password' id='password' className="form-control"/>
+                                    <input type='password' name='password' id='password' className="form-control mb-0" value={password} onChange={(e) => setPassword(e.target.value) }/>
+                                    {
+                                        <label className='text-danger'>{passwordStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-6">
                                 <div className="field-set">
                                     <label>Re-enter Password:</label>
-                                    <input type='text' name='re-password' id='re-password' className="form-control"/>
+                                    <input type='password' name='re-password' id='re-password' className="form-control mb-0" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                                                                        {
+                                        <label className='text-danger'>{confirmPasswordStatus}</label>
+                                    }
                                 </div>
                             </div>
 
                             <div className="col-md-12">
-                                <div id='submit' className="pull-left">
-                                    <input type='submit' id='send_message' value='Register Now' className="btn btn-main color-2" />
+                                <div id='submit' className="pull-left mt-4">
+                                    <input type='submit' id='send_message' value='Register Now' className="btn btn-main color-2" onClick={register}/>
                                 </div>
                                 
                                 <div className="clearfix"></div>
                             </div>
 
                         </div>
-                    </form>
+                    </div>
                 </div>
 
                 </div>
