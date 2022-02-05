e8183dbd7fcfcef2a2cd28ba219f962e3f8e8a07
climax-solution <blocklimax@gmail.com>
2022-02-06 01:41:50 +0200

    update log table

---
+++ backend/models/activity-log.js
@@ -0,0 +1,36 @@
+let Mongoose = require('mongoose');
+let { Schema } = Mongoose;
+
+const ActivityLogs = Schema({
+    walletAddress :{
+        type: String
+    },
+    tokenID: {
+        type: String
+    },
+    type: {
+        type: String,
+        enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    },
+    price: {
+        type: String
+    },
+    created_at:{
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = Mongoose.model('activity_logs', ActivityLogs);
+
+// 0 buy
+// 1 on sell
+// 2 down sell
+// 3 to premium
+// 4 to normal
+// 5 on auction
+// 6 down auction
+// 7 make bid
+// 8 cancel bid
+// 9 like
+// 10 dislike
+// 11 claim
