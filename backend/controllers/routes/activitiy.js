ae2c026fa7a2b321b6ee5f9c216ce1c9d4d6a08f
climax-solution <blocklimax@gmail.com>
2022-02-08 14:42:47 +0200

    add get activity log feature

--- backend/controllers/routes/activitiy.js
+++ backend/controllers/routes/activitiy.js
@@ -20,13 +20,18 @@ router.post('/create-log', async(req, res) => {
     })
 });
 
-router.post('/get-logs', async(req, res) => {
-    const { type } = req.body;
+router.get('/get-logs', async(req, res) => {
+    const { b, type } = req.query;
     const query = type ? { type } : {};
-    const list = await ActivitySchema.find(query);
-    res.status(200).json({
-        list
-    });
+    let list = await ActivitySchema.find(query);
+    list = list.slice(Number(b), Number(b) + 5);
+    for await (let item of list) {
+        let { avatar } = await UserSchema.findOne({ walletAddress: item.walletAddress });
+        if (!avatar) avatar = "empty-avatar.png";
+        item._doc = { ...item._doc, avatar: avatar};
+    }
+
+    res.status(200).json(list);
 });
 
 module.exports = router;
