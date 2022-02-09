6cf8fdbeebc19061ca06756a19cb1e3683d3402a
climax-solution <blocklimax@gmail.com>
2022-02-09 15:21:41 +0200

    add loading

--- frontend/src/components/components/CarouselNew.js
+++ frontend/src/components/components/CarouselNew.js
@@ -6,6 +6,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import Clock from "./Clock";
 import Empty from "./Empty";
 import getWeb3 from "../../utils/getWeb3";
+import PremiumNFTLoading from './Loading/PremiumNFTLoading';
 
 const Outer = styled.div`
   display: flex;
@@ -25,6 +26,7 @@ const GlobalStyles = createGlobalStyle`
 export default function ({ data }) {
   const [list, setList] = useState([]);
   const [web3, setWEB3] = useState([]);
+  const [carouselLoading, setCarouselLoading] = useState(true);
 
   const settings = {
     dots: false,
@@ -78,6 +80,7 @@ export default function ({ data }) {
   useEffect(async () => {
     const { _web3 } = await getWeb3();
     setWEB3(_web3);
+    setCarouselLoading(false);
   },[])
   useEffect(() => {
     setList(data);
@@ -86,53 +89,58 @@ export default function ({ data }) {
   return (
     <div className='nft'>
       <GlobalStyles/>
-      <Slider {...settings}>
-        {
-          list.map((item, index) => {
-            const { auctionData: auction, marketData: market } = item;
-            const price = !auction.existance ? market.price : (auction.currentBidPrice ? auction.currentBidPrice : auction.minPrice);
-            return (
-              <div className='itm' key={index}>
-                <div className="d-item">
-                    <div className="nft__item">
-                        <div className="de_countdown">
-                          {/* <Clock deadline={this.state.deadline2} /> */}
-                        </div>
-                        <div className="author_list_pp">
-                            <span onClick={()=> window.open("/#", "_self")}>                                    
-                                <img className="lazy" src="./img/author/author-2.jpg" alt=""/>
-                                <i className="fa fa-check"></i>
-                            </span>
-                        </div>
-                        <div className="nft__item_wrap">
-                          <Outer>
-                            <span>
-                                <img src={item.image} className="lazy nft__item_preview" alt=""/>
-                            </span>
-                          </Outer>
-                        </div>
-                        <div className="nft__item_info">
-                            <span onClick={()=> window.open("/#", "_self")}>
-                                <h4>{item.nftName}</h4>
-                            </span>
-                            <div className="nft__item_price">
-                                {web3.utils.fromWei(price)} BNB
-                            </div>    
-                            <div className="nft__item_action">
-                                <span onClick={()=> window.open("/#", "_self")}>Place a bid</span>
+      { carouselLoading && <PremiumNFTLoading/> }
+      {
+        !carouselLoading && (
+          <Slider {...settings}>
+            {
+              list.map((item, index) => {
+                const { auctionData: auction, marketData: market } = item;
+                const price = !auction.existance ? market.price : (auction.currentBidPrice ? auction.currentBidPrice : auction.minPrice);
+                return (
+                  <div className='itm' key={index}>
+                    <div className="d-item">
+                        <div className="nft__item">
+                            <div className="de_countdown">
+                              {/* <Clock deadline={this.state.deadline2} /> */}
                             </div>
-                            <div className="nft__item_like">
-                                <i className="fa fa-heart"></i><span>45</span>
-                            </div>                                  
-                        </div> 
+                            <div className="author_list_pp">
+                                <span onClick={()=> window.open("/#", "_self")}>                                    
+                                    <img className="lazy" src="./img/author/author-2.jpg" alt=""/>
+                                    <i className="fa fa-check"></i>
+                                </span>
+                            </div>
+                            <div className="nft__item_wrap">
+                              <Outer>
+                                <span>
+                                    <img src={item.image} className="lazy nft__item_preview" alt=""/>
+                                </span>
+                              </Outer>
+                            </div>
+                            <div className="nft__item_info">
+                                <span onClick={()=> window.open("/#", "_self")}>
+                                    <h4>{item.nftName}</h4>
+                                </span>
+                                <div className="nft__item_price">
+                                    {web3.utils.fromWei(price)} BNB
+                                </div>    
+                                <div className="nft__item_action">
+                                    <span onClick={()=> window.open("/#", "_self")}>Place a bid</span>
+                                </div>
+                                <div className="nft__item_like">
+                                    <i className="fa fa-heart"></i><span>45</span>
+                                </div>                                  
+                            </div> 
+                        </div>
                     </div>
-                </div>
-              </div>
-            )
-          })
-        }
-      </Slider>
-      { !list.length && <Empty/> }
+                  </div>
+                )
+              })
+            }
+          </Slider>
+        )
+      }
+      { !list.length && !carouselLoading && <Empty/> }
     </div>
   )
 }
