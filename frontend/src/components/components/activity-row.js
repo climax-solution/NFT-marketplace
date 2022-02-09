6dbd34c57e6b7d4b0271377879faf1f6901bfc9b
climax-solution <blocklimax@gmail.com>
2022-02-09 16:12:40 +0200

    fix image size

--- frontend/src/components/components/activity-row.js
+++ frontend/src/components/components/activity-row.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
+import { createGlobalStyle } from "styled-components";
 
 const action = [
     ["action-buy","Buy"],
@@ -18,18 +19,27 @@ const action = [
     ["action-disfollow","Disfollow"]
 ];
 
+const GlobalStyles = createGlobalStyle`
+    .ratio-1-1 {
+        aspect-ratio: 1;
+    }
+`;
+
 const ActivityItem = ({ data }) => {
     return (
-        <li className={action[data["type"]][0]}>
-          <img className="lazy" src={`http://localhost:7060/avatar/${data.avatar}`} alt="" crossOrigin="true"/>
-          <div className="act_list_text">
-              <h4>{action[data["type"]][1]}</h4>
-             1 edition purchased by <span className='color'>{data.walletAddress}</span>
-              <span className="act_list_date">
-                  {moment(data.created_at).format('MMMM Do YYYY, h:mm:ss a')}
-              </span>
-          </div>
-        </li>
+        <>
+            <GlobalStyles/>
+            <li className={action[data["type"]][0]}>
+            <img className="lazy ratio-1-1" src={`http://localhost:7060/avatar/${data.avatar}`} alt="" crossOrigin="true"/>
+            <div className="act_list_text">
+                <h4>{action[data["type"]][1]}</h4>
+                1 edition purchased by <span className='color'>{data.walletAddress}</span>
+                <span className="act_list_date">
+                    {moment(data.created_at).format('MMMM Do YYYY, h:mm:ss a')}
+                </span>
+            </div>
+            </li>
+        </>
     )
 }
 
