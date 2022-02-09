bf3bc8d01fd209f4c1d1dfb857b11b7df72b9065
climax-solution <blocklimax@gmail.com>
2022-02-09 23:30:07 +0200

    add top seller list

--- frontend/src/components/components/authorList.js
+++ frontend/src/components/components/authorList.js
@@ -1,153 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getWeb3 from '../../utils/getWeb3';
+import Empty from './Empty';
+import TopSellerLoading from './Loading/TopSellerLoading';
 
-const authorlist= () => (
-  <div>
-    <ol className="author_list">
-      <li>                                    
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>
-                  <img className="lazy" src="./img/author/author-1.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>                                    
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Monica Lucas</span>
-              <span className="bot">3.2 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-2.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Mamie Barnett</span>
-              <span className="bot">2.8 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-3.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Nicholas Daniels</span>
-              <span className="bot">2.5 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-4.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Lori Hart</span>
-              <span className="bot">2.2 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-5.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Jimmy Wright</span>
-              <span className="bot">1.9 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-6.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Karla Sharp</span>
-              <span className="bot">1.6 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-7.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Gayle Hicks</span>
-              <span className="bot">1.5 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-8.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Claude Banks</span>
-              <span className="bot">1.3 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-9.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Franklin Greer</span>
-              <span className='bot'>0.9 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-10.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Stacy Long</span>
-              <span className='bot'>0.8 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-11.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Ida Chapman</span>
-              <span className='bot'>0.6 ETH</span>
-          </div>
-      </li>
-      <li>
-          <div className="author_list_pp">
-              <span onClick={()=> window.open("", "_self")}>                                    
-                  <img className="lazy" src="./img/author/author-12.jpg" alt=""/>
-                  <i className="fa fa-check"></i>
-              </span>
-          </div>
-          <div className="author_list_info">
-              <span onClick={()=> window.open("", "_self")}>Fred Ryan</span>
-              <span className='bot'>0.5 eth</span>
-          </div>
-      </li>
-    </ol>
-  </div>
-);
+const authorlist= ({ data }) => {
+    
+    const [list, setList] = useState([]);
+    const [web3,setWEB3] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setList(data);
+    },[data]);
+
+    useEffect(async() => {
+        const { _web3 } = await getWeb3();
+        setWEB3(_web3);
+        setLoaded(true);
+    },[])
+    return (
+        <>
+            { !loaded && <TopSellerLoading/> }
+            {
+                loaded && (
+                    <div>
+                        <ol className="author_list">
+                            {
+                                list.map((item, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <div className="author_list_pp">
+                                                <span>
+                                                    <img className="lazy ratio-1-1" src={`http://localhost:7060/avatar/${item?.avatar}`} alt="" crossOrigin='true'/>
+                                                    <i className="fa fa-check"></i>
+                                                </span>
+                                            </div>                                    
+                                            <div className="author_list_info">
+                                                <span>{item.firstName + "  " + item.lastName}</span>
+                                                <span className="bot">{web3.utils.fromWei((item.price).toString(), "ether")} ETH</span>
+                                            </div>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ol>
+                    </div>
+                )
+            }
+
+            {
+                loaded && !list.length && <Empty/>
+            }
+        </>
+    )
+}
 export default authorlist;
