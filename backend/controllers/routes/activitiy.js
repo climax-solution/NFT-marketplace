01f5fe3e3e86aa6e05c764021aeabf96373f3350
climax-solution <blocklimax@gmail.com>
2022-02-09 23:29:05 +0200

    add get likes number and top sellers

--- backend/controllers/routes/activitiy.js
+++ backend/controllers/routes/activitiy.js
@@ -34,4 +34,59 @@ router.get('/get-logs', async(req, res) => {
     res.status(200).json(list);
 });
 
+router.post('/get-likes', async(req,res) => {
+    const { tokenID, walletAddress } = req.body;
+    try {
+        const liked = await ActivitySchema.count({ tokenID: tokenID, type: "9" });
+        const disLiked = await ActivitySchema.count({ tokenID: tokenID, type: "10" });
+        const lastAct = await ActivitySchema.find({ walletAddress, tokenID, type: { $in: ["9", "10"]} }, {}, { sort: { 'created_at' : 1 } })
+        res.status(200).json({
+            liked: (liked - disLiked) < 0 ? 0 : (liked - disLiked),
+            lastAct: lastAct.length ? lastAct[lastAct.length - 1].type : "10"
+        });
+
+    } catch(err) {
+        res.status(200).json({
+            count: 0,
+            lastAct: "10"
+        })
+    }
+})
+
+router.post('/get-top-sellers', async(req, res) => {
+    try {
+        let seller = await ActivitySchema.aggregate([
+            {
+                $match: {
+                    type: "0"
+                }
+            },
+            {
+                $group:
+                   {
+                    _id: "$walletAddress",
+                    price: { $sum: "$price" }
+                }
+            }
+        ]).sort({ price: 'desc', _id: 'desc'});
+        
+        let list = [];
+        for await (let item of seller) {
+            let user = await UserSchema.findOne({ walletAddress: item._id });
+            if (!user) {
+                item = {
+                    avatar: "empty-avatar.png",
+                    firstName: "----",
+                    lastName: "---",
+                    ...item
+                };
+            }
+            list.push({ ...user?._doc, ...item});
+        }
+
+        res.status(200).json(list);
+    } catch(err) {
+        console.log(err);
+    }
+})
 module.exports = router;
