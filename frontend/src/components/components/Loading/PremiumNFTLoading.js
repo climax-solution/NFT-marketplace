424740d8220fe951d5d9a1a280fc5418ff65c659
climax-solution <blocklimax@gmail.com>
2022-02-27 17:28:22 +0200

    update loadings

--- frontend/src/components/components/Loading/PremiumNFTLoading.js
+++ frontend/src/components/components/Loading/PremiumNFTLoading.js
@@ -1,77 +1,88 @@
 import Skeleton from "react-loading-skeleton";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    .react-loading-skeleton {
+        background-color: #2a2b2c !important;
+        background-image: linear-gradient(90deg ,#2a2b2c,#444,#2a2b2c ) !important;
+    }
+`;
 
 const PremiumNFTLoading = () => {
     return (
-        <div className="row">
-            <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
-                <div className="nft__item">
-                    <div className="nft__item_wrap">
-                        <span>
-                            <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
-                        </span>
+        <>
+            <GlobalStyles/>
+            <div className="row">
+                <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
+                    <div className="nft__item">
+                        <div className="nft__item_wrap">
+                            <span>
+                                <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
+                            </span>
+                        </div>
+                        <div className="nft__item_info">
+                            <span>
+                                <h4><Skeleton/></h4>
+                            </span>
+                            <span className="d-block mb-4">
+                                <Skeleton/>
+                            </span>
+                        </div>
                     </div>
-                    <div className="nft__item_info">
-                        <span>
-                            <h4><Skeleton/></h4>
-                        </span>
-                        <span className="d-block mb-4">
-                            <Skeleton/>
-                        </span>
+                </div>
+                <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
+                    <div className="nft__item">
+                        <div className="nft__item_wrap">
+                            <span>
+                                <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
+                            </span>
+                        </div>
+                        <div className="nft__item_info">
+                            <span>
+                                <h4><Skeleton/></h4>
+                            </span>
+                            <span className="d-block mb-4">
+                                <Skeleton/>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
+                    <div className="nft__item">
+                        <div className="nft__item_wrap">
+                            <span>
+                                <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
+                            </span>
+                        </div>
+                        <div className="nft__item_info">
+                            <span>
+                                <h4><Skeleton/></h4>
+                            </span>
+                            <span className="d-block mb-4">
+                                <Skeleton/>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
+                    <div className="nft__item">
+                        <div className="nft__item_wrap">
+                            <span>
+                                <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
+                            </span>
+                        </div>
+                        <div className="nft__item_info">
+                            <span>
+                                <h4><Skeleton/></h4>
+                            </span>
+                            <span className="d-block mb-4">
+                                <Skeleton/>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
-                <div className="nft__item">
-                    <div className="nft__item_wrap">
-                        <span>
-                            <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
-                        </span>
-                    </div>
-                    <div className="nft__item_info">
-                        <span>
-                            <h4><Skeleton/></h4>
-                        </span>
-                        <span className="d-block mb-4">
-                            <Skeleton/>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
-                <div className="nft__item">
-                    <div className="nft__item_wrap">
-                        <span>
-                            <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
-                        </span>
-                    </div>
-                    <div className="nft__item_info">
-                        <span>
-                            <h4><Skeleton/></h4>
-                        </span>
-                        <span className="d-block mb-4">
-                            <Skeleton/>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3">
-                <div className="nft__item">
-                    <div className="nft__item_wrap">
-                        <span>
-                            <Skeleton className="lazy nft__item_preview ratio ratio-1x1"/>
-                        </span>
-                    </div>
-                    <div className="nft__item_info">
-                        <span>
-                            <h4><Skeleton/></h4>
-                        </span>
-                        <span className="d-block mb-4">
-                            <Skeleton/>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
