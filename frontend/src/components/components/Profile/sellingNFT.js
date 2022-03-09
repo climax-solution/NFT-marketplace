2297d67091a8bc28c5742db95865e9af40774a5c
climax-solution <blocklimax@gmail.com>
2022-03-09 18:11:36 +0200

    add category to selling NFT list

--- frontend/src/components/components/Profile/sellingNFT.js
+++ frontend/src/components/components/Profile/sellingNFT.js
@@ -1,11 +1,43 @@
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import { useSelector } from "react-redux";
+import Select from 'react-select';
 import InfiniteScroll from "react-infinite-scroll-component";
 import getWeb3 from "../../../utils/getWeb3";
 import PremiumNFTLoading from "../Loading/PremiumNFTLoading";
 
 const Empty = lazy(() => import("../Empty"));
 const NFTItem = lazy(() => import("./sellingNFTItem"));
+
+const customStyles = {
+    option: (base, state) => ({
+      ...base,
+      background: "#212428",
+      color: "#fff",
+      borderRadius: state.isFocused ? "0" : 0,
+      "&:hover": {
+        background: "#16181b",
+      }
+    }),
+    menu: base => ({
+      ...base,
+      background: "#212428 !important",
+      borderRadius: 0,
+      marginTop: 0
+    }),
+    menuList: base => ({
+      ...base,
+      padding: 0
+    }),
+    control: (base, state) => ({
+      ...base,
+      padding: 2
+    })
+};
+
+const categories = [
+    { "value": 0, "label": "Normal NFTs" },
+    { "value": 1, "label": "Premium NFTs"},
+];
 
 export default function SellingNFT() {
 
@@ -14,20 +46,25 @@ export default function SellingNFT() {
     const [nfts, setNFTs] = useState([]);
     const [nftContract, setNFTContract] = useState({});
     const [marketContract, setMarketContract] = useState({});
+    const [activeCategory, setCategory] = useState({ value: 0, label: "Normal NFTs" });
     const [restList, setRestList] = useState([]);
     const [loaded, setLoaded] = useState(false);
     
     useEffect(async() => {
         const { instanceMarketplace: Marketplace, instanceNFT } = await getWeb3();
         if (Marketplace) {
+            setLoaded(false);
+            let premium = false;
+            if (activeCategory.value) premium = true;
             setNFTContract(instanceNFT);
             setMarketContract(Marketplace);
             let list = await Marketplace.methods.getPersonalNFTList().call({ from: initialUser.walletAddress });
-            list = list.filter(item => item.marketData.existance && item.marketData.marketStatus);
+            list = list.filter(item => item.marketData.existance && item.marketData.marketStatus && item.marketData.premiumStatus == premium);
+            setNFTs([]);
             setRestList(list);
             setLoaded(true);
         }
-    },[])
+    },[activeCategory, ])
   
     useEffect(async() => {
         if (loaded) {
@@ -50,6 +87,16 @@ export default function SellingNFT() {
     return (
         <>
             <Suspense fallback={<PremiumNFTLoading/>}>
+                <Select
+                    className='select1 mx-auto me-0 mx-200px'
+                    styles={customStyles}
+                    menuContainerStyle={{'zIndex': 999}}
+                    value={activeCategory}
+                    options={categories}
+                    onChange={(value) => {
+                        setCategory(value);
+                    }}
+                />
                 <InfiniteScroll
                     dataLength={nfts.length}
                     next={fetchNFT}
