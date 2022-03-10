2c3158f5e267bd28790ba69a7bc18c504fb1b7d8
climax-solution <blocklimax@gmail.com>
2022-03-10 23:01:20 +0200

    add request new email

--- backend/controllers/routes/index.js
+++ backend/controllers/routes/index.js
@@ -2,9 +2,11 @@ let router = require('express').Router();
 let users = require('./users');
 let auth = require('./auth');
 let activity = require('./activitiy');
+let newsletter = require('./externals');
 
 router.use('/user', users);
 router.use('/auth', auth);
 router.use('/activity', activity);
+router.use('/news', newsletter);
 
 module.exports = router;
