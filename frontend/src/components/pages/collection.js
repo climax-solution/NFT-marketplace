87b3c010496d0c15e65a6f8e423692188ac309ad
climax-solution <blocklimax@gmail.com>
2022-03-11 14:57:32 +0200

    remove modules

--- frontend/src/components/pages/collection.js
+++ frontend/src/components/pages/collection.js
@@ -87,9 +87,6 @@ const Collection= function() {
               }
             </InfiniteScroll>
             {
-              !loaded && <Loading/>
-            }
-            {
               loaded && !nfts.length && !restList.length && <Empty/>
             }
         </section>
