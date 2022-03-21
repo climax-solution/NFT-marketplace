8095a9d30a5d9ffac6f2547e61ed769d2457a7b2
climax-solution <blocklimax@gmail.com>
2022-03-21 14:13:05 +0200

    change component name NotsellingNFT -> Collected NFT

--- frontend/src/components/components/Profile/Collected/CollectedNFT.js
+++ frontend/src/components/components/Profile/Collected/CollectedNFT.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, lazy } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useSelector } from "react-redux";
-import NotSaleNFT from "./notSellingNFTItem";
+import NotSaleNFT from "./collectedNFTItem";
 import getWeb3 from "../../../../utils/getWeb3";
 import axios from "axios";
 
 const Empty = lazy(() => import("../../Empty"));
 const PremiumNFTLoading = lazy(() => import("../../Loading/PremiumNFTLoading"));
 
-export default function NotSellingNFT() {
+export default function CollectedNFT() {
 
     const initUser = useSelector((state) => state.auth.user);
 
