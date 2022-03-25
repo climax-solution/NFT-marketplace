2049585879e393c52e80d07e973c4b392d146736
climax-solution <blocklimax@gmail.com>
2022-03-25 14:22:51 +0200

    update smart contract

--- smart contract/contracts/PhotoMarketplace.sol
+++ smart contract/contracts/PhotoMarketplace.sol
@@ -14,7 +14,7 @@ contract Marketplace is Ownable{
 
     string public constant salt = "NFTD MARKETPLACE";
     mapping(address => bool) public whitelist;
-    mapping(address => uint) public nonces;
+    mapping(uint => uint) public nonces;
 
     NFTD public flexNFT;
     // IERC20 public WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); //mainnet weth
@@ -47,7 +47,7 @@ contract Marketplace is Ownable{
         address to = msg.sender;
         require(msg.value >= price, "insufficient price");
         require(flexNFT.ownerOf(tokenId) == from, "wrong owner");
-        bytes32 digest = keccak256(abi.encodePacked(uint8(0x19), uint8(0x01), DOMAIN_SEPARATOR, keccak256(abi.encode(BUY_SALT, nonces[from] ++, from, tokenId, price, uint256(is_premium ? 1: 0)))));
+        bytes32 digest = keccak256(abi.encodePacked(uint8(0x19), uint8(0x01), DOMAIN_SEPARATOR, keccak256(abi.encode(BUY_SALT, nonces[tokenId] ++, from, tokenId, price, uint256(is_premium ? 1: 0)))));
         address recoveredAddress = ECDSA.recover(digest, signature);
         require(recoveredAddress == from, "invalid signature");
         
@@ -73,7 +73,7 @@ contract Marketplace is Ownable{
         require(WETH.balanceOf(to) >= price, "payer doen't have enough price");
         require(flexNFT.ownerOf(tokenId) == from, "wrong owner");
 
-        bytes32 digest = keccak256(abi.encodePacked(uint8(0x19), uint8(0x01), DOMAIN_SEPARATOR, keccak256(abi.encode(SELL_SALT, nonces[to] ++, to, tokenId, price))));
+        bytes32 digest = keccak256(abi.encodePacked(uint8(0x19), uint8(0x01), DOMAIN_SEPARATOR, keccak256(abi.encode(SELL_SALT, nonces[tokenId] ++, to, tokenId, price))));
         address recoveredAddress = ECDSA.recover(digest, signature);
         require(recoveredAddress == to, "invalid signature");
         flexNFT.transferFrom(from, to, tokenId);
