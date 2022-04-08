df605f6f0181089133d452c9bb1f8fc83d167ff7
climax-solution <blocklimax@gmail.com>
2022-04-08 16:19:55 +0300

    split row

--- frontend/src/components/components/Profile/Bid/BidView.js
+++ frontend/src/components/components/Profile/Bid/BidView.js
@@ -2,17 +2,15 @@ import axios from "axios";
 import { lazy, useEffect, useState } from "react"
 import Skeleton from "react-loading-skeleton";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import getWeb3 from "../../../../utils/getWeb3";
-import { marketplace_addr } from "../../../../config/address.json";
-import { success_toastify, error_toastify } from "../../../../utils/notify";
 
 const Art = lazy(() => import("../../Asset/art"));
+const BidRow = lazy(() => import( "./BidRow"));
 
 export default function BidView() {
 
     const initialUser = useSelector((state) => state.auth.user);
-    const navigate = useNavigate();
 
     const [bidList, setBidList] = useState([]);
     const [isLoading, setLoading] = useState(true);
@@ -20,15 +18,11 @@ export default function BidView() {
     const [isOwner, setOwner] = useState(false);
 
     const [web3, setWeb3] = useState({});
-    const [Marketplace, setMarekplace] = useState({});
-    const [WBNB, setWBNB] = useState({});
     const { tokenID } = useParams();
 
     useEffect(async() => {
-        const { instanceNFT, instanceMarketplace, _web3, instanceWBNB } = await getWeb3();
+        const { instanceNFT, _web3 } = await getWeb3();
         setWeb3(_web3);
-        setWBNB(instanceWBNB);
-        setMarekplace(instanceMarketplace);
         
         const _orgNFT = await instanceNFT.methods.getItemNFT(tokenID).call();
         await axios.get(_orgNFT.tokenURI).then(res => {
@@ -44,49 +38,6 @@ export default function BidView() {
         });
         setLoading(false);
     },[]);
-
-    const accept = async(index) => {
-        try {
-            setLoading(true);
-            const status = await axios.post(`${process.env.REACT_APP_BACKEND}sale/get-nft-item`, { tokenID }).then(res => {
-                const { nft: _nft } = res.data;
-                if (_nft.action == "auction") return _nft.status == "premium" ? true : false;
-                else return false;
-            });
-            
-            if (!status) throw Error();
-
-            await axios.post(`${process.env.REACT_APP_BACKEND}sale/get-bid-item`, {id: bidList[index]._id}).then(async(res) => {
-                const { bid } = res.data;
-                const bidderBalance = await WBNB.methods.balanceOf(bid.walletAddress).call();
-
-                const bidderAllowance = await WBNB.methods.allowance(bid.walletAddress, marketplace_addr).call();
-                if (bidderBalance * 1 < bid.price * 1 || bidderAllowance * 1 < bid.price * 1) throw Error("This user's WBNB balance is not enough to accept");
-                await Marketplace.methods.sell(tokenID, bid.walletAddress, bid.price, status, bid.signature).send({
-                    from: initialUser.walletAddress
-                });
-
-                await axios.post(`${process.env.REACT_APP_BACKEND}sale/accept-offer`, {
-                    tokenID, bidder: bid.walletAddress
-                }).then(res => {
-
-                }).catch(err => {
-
-                });
-                success_toastify("Accepted bid");
-                navigate('/explore');
-            }).catch(err => {
-                throw Error(err.reponse?.data?.err ? err.reponse.data.err : err.message );
-            });
-        } catch(err) {
-            let message = 'Failed';
-            const parsed = JSON.parse(JSON.stringify(err));
-            if (parsed.code == 4001) message = "Canceled";
-
-            error_toastify(message);
-        }
-        setLoading(false);
-    }
 
     return (
         <>
@@ -122,10 +73,10 @@ export default function BidView() {
                         <table className="table table-dark table-bordered table-responsive text-center">
                             <thead>
                                 <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Bidder</th>
-                                { isOwner ? <th scope="col">Action</th> : "" }
+                                    <th scope="col">#</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Bidder</th>
+                                    { isOwner ? <th scope="col">Action</th> : "" }
                                 </tr>
                             </thead>
                             <tbody>
@@ -138,14 +89,16 @@ export default function BidView() {
                                         </>
                                     ) : <>
                                         {
-                                            bidList.map((item, index) => {
+                                            bidList.map((item, idx) => {
                                                 return (
-                                                    <tr key={index}>
-                                                        <td>{index + 1}</td>
-                                                        <td>{web3.utils.fromWei(item.price, 'ether')} BNB</td>
-                                                        <td>{(item.walletAddress).substr(0, 6) + '...' + (item.walletAddress).substr(-4)}</td>
-                                                        { isOwner ? <td className="text-center"><button className="btn-main d-inline-block" onClick={() => accept(index)}>Accept</button></td> : ""}
-                                                    </tr>
+                                                    <BidRow
+                                                        key={idx}
+                                                        index={idx + 1}
+                                                        data={item}
+                                                        isOwner={isOwner}
+                                                        startLoading={setLoading}
+                                                        price={web3.utils.fromWei(item.price, 'ether')}
+                                                    />
                                                 )
                                             })
                                         }
