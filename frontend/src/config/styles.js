9806433664c64739c0ceb566eedde73a9773fb7a
climax-solution <blocklimax@gmail.com>
2022-05-02 21:15:24 +0300

    add filter to folder page

---
+++ frontend/src/config/styles.js
@@ -0,0 +1,25 @@
+export const filterDropdown = {
+    option: (base, state) => ({
+      ...base,
+      background: "#212428",
+      color: "#fff",
+      borderRadius: state.isFocused ? "0" : 0,
+      "&:hover": {
+        background: "#16181b",
+      }
+    }),
+    menu: base => ({
+      ...base,
+      background: "#212428 !important",
+      borderRadius: 0,
+      marginTop: 0
+    }),
+    menuList: base => ({
+      ...base,
+      padding: 0
+    }),
+    control: (base, state) => ({
+      ...base,
+      padding: 2
+    })
+};
