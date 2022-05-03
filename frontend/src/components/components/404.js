ed14702a606be5b2193cc7bf8712b0c95ac7ae4a
climax-solution <blocklimax@gmail.com>
2022-05-03 15:51:45 +0300

    fix 404 page

--- frontend/src/components/components/404.js
+++ frontend/src/components/components/404.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+    .wrapper-404 {
+        min-height: 100vh;
+        @media screen and (max-height: 500px) {
+            min-height: 500px;
+        }
+    }
     .not-found-container {
         position: absolute;
         left: 50%;
@@ -12,10 +18,12 @@ const GlobalStyles = createGlobalStyle`
 
 const notfound = () => {
     return (
-        <div className="not-found-container">
-            <GlobalStyles/>
-            <h1 className="text-white text-center">404</h1>
-            <p className="text-center">Page not found</p>
+        <div className="container wrapper-404 position-relative">
+            <div className="not-found-container">
+                <GlobalStyles/>
+                <h1 className="text-white text-center">404</h1>
+                <p className="text-center">Page not found</p>
+            </div>
         </div>
     )
 }
