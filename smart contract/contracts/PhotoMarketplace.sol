57d06e797790e0940f775df0d1aaf388cac83b46
climax-solution <blocklimax@gmail.com>
2022-05-10 16:54:57 +0300

    add profit function

--- smart contract/contracts/PhotoMarketplace.sol
+++ smart contract/contracts/PhotoMarketplace.sol
@@ -12,9 +12,16 @@ contract Marketplace is Ownable{
 
     using SafeMath for uint256;
 
+    struct Profit {
+        bool status;
+        uint256 owner;
+        uint256 dev;
+    }
+
     string public constant salt = "NFTD MARKETPLACE";
     mapping(uint => uint) public nonces;
     mapping(address => bool) public whitelist;
+    mapping(uint256 => Profit) private profit;
 
     NFTD public flexNFT;
     // IERC20 public WBNB = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); //mainnet weth
@@ -23,6 +30,7 @@ contract Marketplace is Ownable{
     uint public fee = 250;  // 2.5%
     uint public premium_fee = 300;  // 3%
     address public treasurer;  //fee collector
+    address public devWallet; // dev fee collector
 
     bytes32 public immutable DOMAIN_SEPARATOR;
     bytes32 public immutable BUY_SALT;
@@ -54,22 +62,34 @@ contract Marketplace is Ownable{
         flexNFT.transferFrom(from, to, tokenId);
         uint feeValue;
         uint royaltyFee;
-        NFTD.Royalty memory royalty = flexNFT.getRoyalty(tokenId);
-        if (royalty.fee > 0) royaltyFee = msg.value.mul(royalty.fee).div(10000);
+        
         if(is_premium) {
             feeValue = msg.value.mul(premium_fee).div(10000);
         } else {
             feeValue = msg.value.mul(fee).div(10000);
         }
+        
+        if (!profit[tokenId].status) {
+            NFTD.Royalty memory royalty = flexNFT.getRoyalty(tokenId);
+            if (royalty.fee > 0) royaltyFee = msg.value.mul(royalty.fee).div(10000);
+            
+            if(feeValue > 0) {
+                address receiver = treasurer;
+                if (whitelist[msg.sender]) receiver = msg.sender;
+                payable(receiver).transfer(feeValue);
+            }
 
-        if(feeValue > 0) {
-            address receiver = treasurer;
-            if (whitelist[msg.sender]) receiver = msg.sender;
-            payable(receiver).transfer(feeValue);
+            if(royaltyFee > 0) payable(royalty.receiver).transfer(royaltyFee);
         }
 
-        if(royaltyFee > 0) payable(royalty.receiver).transfer(royaltyFee);
+        else {
+            uint256 forOwner = feeValue * profit[tokenId].owner / 100;
+            payable(flexNFT.ownerOf(tokenId)).transfer(forOwner);
+            payable(devWallet).transfer(feeValue - forOwner);
+        }
+
         payable(from).transfer(msg.value - feeValue - royaltyFee);
+        
     }
 
     function sell(uint tokenId, address to, uint price, bool is_premium, bytes memory signature) external {
@@ -83,20 +103,34 @@ contract Marketplace is Ownable{
         flexNFT.transferFrom(from, to, tokenId);
         uint feeValue;
         uint royaltyFee;
-        NFTD.Royalty memory royalty = flexNFT.getRoyalty(tokenId);
-        if(royalty.fee > 0) royaltyFee = price.mul(royalty.fee).div(10000);
+
         if(is_premium) {
             feeValue = price.mul(premium_fee).div(10000);
         } else {
             feeValue = price.mul(fee).div(10000);
         }
-        if(feeValue > 0) {
-            address receiver = treasurer;
-            if (whitelist[msg.sender]) receiver = msg.sender;
-            WBNB.transferFrom(to, receiver, feeValue);
+        
+        if (!profit[tokenId].status) {
+            NFTD.Royalty memory royalty = flexNFT.getRoyalty(tokenId);
+            if (royalty.fee > 0) royaltyFee = price.mul(royalty.fee).div(10000);
+            
+            if(feeValue > 0) {
+                address receiver = treasurer;
+                if (whitelist[msg.sender]) receiver = msg.sender;
+                WBNB.transferFrom(to, receiver, feeValue);
+            }
+
+            if(royaltyFee > 0) WBNB.transferFrom(to, royalty.receiver, royaltyFee);
         }
-        if(royaltyFee > 0) WBNB.transferFrom(to, royalty.receiver, royaltyFee);
+
+        else {
+            uint256 forOwner = feeValue * profit[tokenId].owner / 100;
+            WBNB.transferFrom(to, flexNFT.ownerOf(tokenId), forOwner);
+            WBNB.transferFrom(to, devWallet, feeValue - forOwner);
+        }
+
         WBNB.transferFrom(to, from, price - feeValue - royaltyFee);
+
     }
 
     function setFee(uint _fee, uint _premium_fee) external onlyOwner{
@@ -116,5 +150,28 @@ contract Marketplace is Ownable{
     function removeWhitelist(address account) external onlyOwner {
         require(account != address(0), "Not allowed zero adddress");
         whitelist[account] = false;
+    }
+
+    function setProfit(uint256 tokenID) external onlyOwner {
+        require(!profit[tokenID].status, "You have already set");
+        Profit memory _profit = Profit(true, 40, 60);
+        profit[tokenID] = _profit;
+    }
+
+    function removeProfit(uint256 tokenID) external onlyOwner {
+        require(profit[tokenID].status, "You haven't set");
+        profit[tokenID].status = false;
+    }
+
+    function updateDevWallet(address account) external onlyOwner {
+        require(account != address(0), "invalid address");
+        require(account != devWallet, "same address");
+        devWallet = account;
+    }
+
+    function updateProfitDiv(uint256 tokenID, uint256 owner) external onlyOwner {
+        require(profit[tokenID].status, "you need to set profit");
+        Profit memory _profit = Profit(true, owner, 100 - owner);
+        profit[tokenID] = _profit;
     }
 }
