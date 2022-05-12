e5ec371deb429ed44c9cfe6a0dec42fd0891528f
climax-solution <blocklimax@gmail.com>
2022-05-12 02:01:22 +0200

    reduce notify option

--- frontend/src/utils/notify.js
+++ frontend/src/utils/notify.js
@@ -1,53 +1,27 @@
 import { toast } from "react-toastify";
+const options = {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored"
+};
 
 export const error_toastify = (message) => {
-    toast.error(message, {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored"
-    });
+    toast.error(message, options);
 }
 
 export const success_toastify = (message) => {
-    toast.success(message, {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored"
-    });
+    toast.success(message, options);
 }
 
 export const info_toastify = (message) => {
-    toast.info(message, {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored"
-    });
+    toast.info(message, options);
 }
 
 export const warning_toastify = (message) => {
-    toast.warning(message, {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored"
-    });
+    toast.warning(message, options);
 }
