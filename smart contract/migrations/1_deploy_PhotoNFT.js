ed11d8686fa420f7233ced59bebfd4a803cd8970
climax-solution <blocklimax@gmail.com>
2022-05-13 02:32:44 +0200

    update migration js

--- smart contract/migrations/1_deploy_PhotoNFT.js
+++ smart contract/migrations/1_deploy_PhotoNFT.js
@@ -1,7 +1,5 @@
 const PhotoNFT = artifacts.require("NFTD");
-// const owner = "0xCdE2C94E148227c5b3832E0fA31207326D35ea0e";
-const owner = "0x0F09aE2ba91449a7B4201721f98f482cAF9737Ee";
 
 module.exports = async function(deployer) {
-    // await deployer.deploy(PhotoNFT, owner);
+    // await deployer.deploy(PhotoNFT);
 };
--- smart contract/migrations/2_deploy_PhotoMarketplace.js
+++ smart contract/migrations/2_deploy_PhotoMarketplace.js
@@ -1,7 +1,5 @@
 const PhotoMarketplace = artifacts.require("Marketplace");
-const NFTAddress = "0xBF89f6B7b4C4239A3E939567D8aaa4d6B33Ebefc";
-// const _marketplaceOwner = '0xCdE2C94E148227c5b3832E0fA31207326D35ea0e';
-// const _whiteUser = '0xebc6B3c6F7724BB214b7CF5994078BB883208a98';
+const NFTAddress = "0xc6130D74BA424cb91a500dA5153420d91Ad751f2";
 
 module.exports = async function(deployer) {
     await deployer.deploy(PhotoMarketplace, NFTAddress);
