2fb6b3c6bb388785ad24b480c8debf74651d9957
climax-solution <blocklimax@gmail.com>
2022-06-04 11:51:27 +0300

    add filter to user page

--- frontend/src/components/pages/Users/index.js
+++ frontend/src/components/pages/Users/index.js
@@ -1,36 +1,44 @@
 import React, { useEffect, useState } from 'react';
-// import Select from 'react-select';
 import axios from 'axios';
 import UserList from '../../components/Users/UserList';
 import PremiumNFTLoading from  '../../components/Loading/PremiumNFTLoading';
+import { createGlobalStyle } from 'styled-components';
+
+import style from './style';
+const GlobalStyles = createGlobalStyle`${style}`;
 
 const Users = () => {
+  const [orginList, setOriginList] = useState([]);
   const [userList, setUserList] = useState([]);
-  const [activeCategory, setCategory] = useState({ value:'', label: 'All categories' });
   const [searchKwd, setRealKwd] = useState('');
-  const [tmpKwd, setTmpKwd] = useState('');
   const [isLoading, setLoading] = useState(false);
 
   useEffect(async() => {
-    await filterFolder();
-  },[activeCategory, searchKwd])
+    await filterUser();
+  },[searchKwd])
 
-  const filterFolder = async() => {
+  const filterUser = async() => {
     setLoading(true);
-    let gradList = await axios.post(`${process.env.REACT_APP_BACKEND}user/get-users-list`).then(res => {
-      return res.data.list;
-    }).catch(err => {
-      return [];
-    });
-
-    // gradList = gradList.filter(item => ((item.name).toLowerCase()).search(searchKwd.toLowerCase()) > -1);
-    // if (activeCategory.value) gradList = gradList.filter(item => item.category == activeCategory.value);
-    setUserList(gradList);
+    if (!searchKwd) {
+      const _list = await axios.post(`${process.env.REACT_APP_BACKEND}user/get-users-list`).then(res => {
+        return res.data.list;
+      }).catch(err => {
+        return [];
+      });  
+      setOriginList(_list);
+      setUserList(_list);
+    }
+    else {
+      const _key = searchKwd.trim().toLowerCase();
+      let gradList = orginList.filter(item => ((item.name).toLowerCase()).search(_key) > -1 || item.username.toLowerCase().search(_key) > -1);
+      setUserList(gradList);
+    }
     setLoading(false);
   }
 
   return(
     <>
+      <GlobalStyles/>
       <section className='jumbotron breadcumb no-bg'>
         <div className='mainbreadcumb'>
           <div className='container'>
@@ -44,48 +52,23 @@ const Users = () => {
       </section>
 
       <section className='container'>
-        {/* <div className='row'>
-          <div className='col-lg-12'>
-              <div className="items_filter justify-content-between">
-                <div className="row form-dark" id="form_quick_search" name="form_quick_search">
-                    <div className="col">
-                        <input
-                          className="form-control"
-                          id="name_1"
-                          name="name_1"
-                          placeholder="search item here..."
-                          type="text"
-                          value={tmpKwd}
-                          onChange={(e) => setTmpKwd(e.target.value)}
-                        />
-                        <button
-                          id="btn-submit"
-                          onClick={() => {
-                            setRealKwd(tmpKwd);
-                            // setUserList([]);
-                          }}
-                        >
-                          <i className="fa fa-search bg-color-secondary"></i>
-                        </button>
-                        <div className="clearfix"></div>
-                    </div>
-                </div>
-                <div className='dropdownSelect one'>
-                  <Select
-                    className='select1'
-                    styles={customStyles}
-                    menuContainerStyle={{'zIndex': 999}}
-                    value={activeCategory}
-                    options={categoryOptions}
-                    onChange={(value) => {
-                      setCategory(value);
-                      // setUserList([]);
-                    }}
-                  />
-                </div>
+        <div className="items_filter justify-content-between">
+          <div className="row ml-0" name="form_quick_search">
+            <div className="input-group w-100 position-relative align-items-center flex-nowrap">
+              <i className='fa fa-search icon position-absolute'/>
+              <input
+                type="text"
+                className="form-control ps-5 mb-0 rounded-pill"
+                id="keyword"
+                name="keyword"
+                placeholder="search user here..."
+                value={searchKwd}
+                onChange={(e) => setRealKwd(e.target.value)}
+              />
             </div>
+            <div className="clearfix"></div>
           </div>
-        </div> */}
+        </div>
         {
           isLoading ? <PremiumNFTLoading/>
           : <UserList data={userList}/>
--- frontend/src/components/pages/Users/style.js
+++ frontend/src/components/pages/Users/style.js
@@ -1,41 +1,10 @@
-/* .items_filter {
-    display: flex;
-    align-items: center;
-}
-    #form_quick_search {
-      height: 42px;
-      top: 0;
-      width: calc(100% - 200px);
-    }
-      .form-control {
-        width: calc(100% - 60px);
-      }
-    }
-    .dropdownSelect {
-      margin-bottom: 0;
-    }
-    @media only screen and (max-width: 768px) {
-      .dropdownSelect {
-        width: 200px;
-      }
-    }
+const style = `
+  .input-group .icon{
+    left: 30px;
+    font-size: 20px;
+    color: #a2a2a2 !important;
+    z-index: 9998;
+  }
+`;
 
-    @media only screen and (max-width: 600px) {
-      flex-direction: column;
-      #form_quick_search {
-        width: 100%;
-        margin: 0;
-        margin-bottom: 20px;
-        .col {
-          padding-left: 0;
-        }
-        #btn-submit {
-          width: 60px;
-        }
-      }
-      .dropdownSelect {
-        width: 100%;
-      }
-      
-    }
-  } */
+export default style;
